2419ad78efa591f323f3b097c0d545a1a5d365f0
M0gM0g <markmoleary@gmail.com>
2018-06-10 20:00:18 -0400

    Add updated game board with clickable boxes
    Make board actually square.
    Also add ability to alternate turns and
    console log for winner.
    
    Need to account for a tie.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,116 @@
 'use strict'
+$(document).ready(function () {
+// default to player one
+  let player = 1
 
+  // event listener for individual boxes
+  $('.col-xs-4').on('click', function (event) {
+    console.log('this worked')
+    // $(this).html('x') -good idea but dont need to change html for this rn
+    // 'this' always becomes the box that you select
+    const boxSelected = $(this)
+
+    // to check if a box is free
+    if (boxSelected.hasClass('X') || boxSelected.hasClass('O')) {
+      console.log('this box has already been taken')
+    } else {
+      // otherwise player 1 can add an x
+      if (player === 1) {
+        boxSelected.addClass('X')
+        if (checkIfWon('X')) {
+          console.log('the winner is ' + player)
+        } else {
+          // check for winner, if no, next player turn
+          player = 2
+        }
+      } else {
+        boxSelected.addClass('O')
+        if (checkIfWon('O')) {
+          console.log('the winner is ' + player)
+        } else {
+          player = 1
+        }
+      }
+    }
+  })
+
+// better way than to check each combination?
+  function checkIfWon (XorO) {
+    if ($('.box1').hasClass(XorO) && $('.box2').hasClass(XorO) && $('.box3').hasClass(XorO)) {
+      return true
+    } else if ($('.box4').hasClass(XorO) && $('.box5').hasClass(XorO) && $('.box6').hasClass(XorO)) {
+      return true
+    } else if ($('.box7').hasClass(XorO) && $('.box8').hasClass(XorO) && $('.box9').hasClass(XorO)) {
+      return true
+    } else if ($('.box1').hasClass(XorO) && $('.box4').hasClass(XorO) && $('.box7').hasClass(XorO)) {
+      return true
+    } else if ($('.box2').hasClass(XorO) && $('.box5').hasClass(XorO) && $('.box8').hasClass(XorO)) {
+      return true
+    } else if ($('.box3').hasClass(XorO) && $('.box6').hasClass(XorO) && $('.box9').hasClass(XorO)) {
+      return true
+    } else if ($('.box1').hasClass(XorO) && $('.box5').hasClass(XorO) && $('.box9').hasClass(XorO)) {
+      return true
+    } else if ($('.box3').hasClass(XorO) && $('.box6').hasClass(XorO) && $('.box9').hasClass(XorO)) {
+      return true
+    } else if ($('.box3').hasClass(XorO) && $('.box5').hasClass(XorO) && $('.box7').hasClass(XorO)) {
+      return true
+    } else {
+      return false
+    }
+  }
+})
+// FIRST TRY
+// const events = require('./events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-// require('./example')
+// require(/example')
+
+// const moveTurn = 1;
+
+// const makeAnX = function () {
+//   $(this).html('X with a function')
+// }
+
+// $('#box-1').on('click', makeAnX)
+//
+// $('#box-2').on('click', function () {
+//   $('#box-2').html('X')
+// })
+//
+// $('#box-3').on('click', function () {
+//   $('#box-3').html('X')
+// })
+//
+// $('#box-4').on('click', function () {
+//   $('#box-4').html('X')
+// })
+//
+// $('#box-5').on('click', function () {
+//   $('#box-5').html('X')
+// })
+//
+// $('#box-6').on('click', function () {
+//   $('#box-6').html('X')
+// })
+//
+// $('#box-7').on('click', function () {
+//   $('#box-7').html('X')
+// })
+//
+// $('#box-8').on('click', function () {
+//   $('#box-8').html('X')
+// })
+//
+// $('#box-9').on('click', function () {
+//   $('#box-9').html('X')
+// })
+
+// $('.col-xs-4').on('click', function () {
+//   $(this).html('X with a function & refactored')
+// })
 
 $(() => {
-  // your JS code goes here
+  // events.addHandlers()
 })
